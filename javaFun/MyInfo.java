66ebd9e171ba0301484738c8d34f784318d2394d
Asbel <asbeln01@yahoo.com>
2021-06-07 12:55:46 -0500

    Java Fun

---
+++ javaFun/MyInfo.java
@@ -0,0 +1,7 @@
+public class MyInfo {
+    public static void main (String[] args){
+        System.out.println("Hi my name is Asbel");
+        System.out.println("I am 37yrs old");
+        System.out.println("My hometown is Eldoret,Kenya");
+    }
+}
