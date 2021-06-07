663160606df039c819208c171c9137480ced2827
Asbel <asbeln01@yahoo.com>
2021-06-07 14:24:23 -0500

    FizzBuzz

---
+++ javaFun/FizzBuzzTest.java
@@ -0,0 +1,12 @@
+public class FizzBuzzTest{
+
+    public static void main(String[] args) {
+
+        FizzBuzz num = new FizzBuzz();
+
+        System.out.println(num.fizzBuzz(15));
+        
+    }
+
+}
+    
