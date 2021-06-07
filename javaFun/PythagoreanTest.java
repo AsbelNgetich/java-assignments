4f958dea74002d22568b08811db0b3cdcc3ea0ff
Asbel <asbeln01@yahoo.com>
2021-06-07 13:39:04 -0500

    Pythagorean assignment

---
+++ javaFun/PythagoreanTest.java
@@ -0,0 +1,11 @@
+public class PythagoreanTest{
+    public static void main(String[] args) {
+
+        Pythagorean hype = new Pythagorean();
+
+        double result = hype.calculateHypotenuse(4,4);
+        System.out.println("The result is....");
+        System.out.println(result);
+        
+    }
+}
